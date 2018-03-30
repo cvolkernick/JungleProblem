2fb1157b94b00f2bc8a70bc63b09899a0fbff18d
Chris Volkernick <cvolkern@gmail.com>
2018-03-30 17:55:04 -0400

    Add random activity action in the Jungle.

--- app/src/main/java/com/example/admin/jungleproblem/model/Jungle.java
+++ app/src/main/java/com/example/admin/jungleproblem/model/Jungle.java
@@ -3,9 +3,11 @@ package com.example.admin.jungleproblem.model;
 import android.util.Log;
 
 import com.example.admin.jungleproblem.model.animals.Animal;
+import com.example.admin.jungleproblem.model.food.Grain;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 import static android.content.ContentValues.TAG;
 
@@ -29,6 +31,26 @@ public class Jungle {
         for (Animal a : animals) {
             a.makeSound();
             Log.d(TAG, "soundOff: " + a.toString());
+        }
+    }
+
+    public void randomActivities() {
+        Random random = new Random();
+
+        for (Animal a : animals) {
+            int actionNumber = random.nextInt(3 - 1) + 1;
+
+            switch (actionNumber) {
+                case 1:
+                    a.eat(new Grain());
+                    break;
+                case 2:
+                    a.sleep();
+                    break;
+                case 3:
+                    a.makeSound();
+                    break;
+            }
         }
     }
 }
